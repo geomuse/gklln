c2899ccf56c0d12e38b1f43b2f0d7f879a8645b3
geo <34396454+geomuse@users.noreply.github.com>
2025-09-05 13:02:15 +0800

    updated

--- _posts/web/example.py
+++ _posts/web/example.py
@@ -1,6 +1,6 @@
 import re 
 
 pattern = re.compile(r"\d{4}-\d{2}-\d{2}")
-text = "今天是 2025-08-02"
-match = pattern.search(text)
-print(match.group())  # 2025-08-02
+text = "今天是 2025-08-02 2025-08-05"
+match = pattern.findall(text)
+print(match)  # 2025-08-02
